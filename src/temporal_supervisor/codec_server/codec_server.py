96dbf98dcdd533d5d5cdb8da37bde1dfb8e2d406
Rick Ross <rick.ross@temporal.io>
2026-06-04 08:32:14 -0400

    Updated codec server to support Temporal Cloud

--- src/temporal_supervisor/codec_server/codec_server.py
+++ src/temporal_supervisor/codec_server/codec_server.py
@@ -18,15 +18,24 @@ from common.redis_config import RedisConfig
 from temporal_supervisor.claim_check.claim_check_codec import ClaimCheckCodec
 
 TEMPORAL_UI_ORIGIN = "http://localhost:8233"
+ALLOWED_ORIGINS = {TEMPORAL_UI_ORIGIN, "https://cloud.temporal.io"}
 
 
 def build_codec_server() -> web.Application:
-    async def cors_options(req: web.Request) -> web.Response:
-        resp = web.Response()
-        if req.headers.get(hdrs.ORIGIN) == TEMPORAL_UI_ORIGIN:
-            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_ORIGIN] = TEMPORAL_UI_ORIGIN
-            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_METHODS] = "POST"
-            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_HEADERS] = "content-type,x-namespace"
+    def set_cors_headers(req: web.Request, resp: web.Response) -> None:
+        origin = req.headers.get(hdrs.ORIGIN)
+        if origin is not None and origin in ALLOWED_ORIGINS:
+            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_ORIGIN] = origin
+            resp.headers[hdrs.VARY] = "Origin"
+            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_METHODS] = "POST, OPTIONS"
+            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_HEADERS] = (
+                "content-type, x-namespace, authorization"
+            )
+            resp.headers[hdrs.ACCESS_CONTROL_ALLOW_CREDENTIALS] = "true"
+
+    async def cors_preflight(req: web.Request) -> web.Response:
+        resp = web.Response(status=204)
+        set_cors_headers(req, resp)
         return resp
 
     async def apply(
@@ -37,9 +46,11 @@ def build_codec_server() -> web.Application:
         data = await req.read()
         payloads = json_format.Parse(data, Payloads())
         payloads = Payloads(payloads=await fn(payloads.payloads))
-        resp = await cors_options(req)
-        resp.content_type = "application/json"
-        resp.text = json_format.MessageToJson(payloads)
+        resp = web.Response(
+            content_type="application/json",
+            text=json_format.MessageToJson(payloads),
+        )
+        set_cors_headers(req, resp)
         return resp
 
     codec = ClaimCheckCodec(config=RedisConfig())
@@ -48,7 +59,8 @@ def build_codec_server() -> web.Application:
         [
             web.post("/encode", partial(apply, codec.encode)),
             web.post("/decode", partial(apply, codec.decode)),
-            web.options("/decode", cors_options),
+            web.options("/encode", cors_preflight),
+            web.options("/decode", cors_preflight),
         ]
     )
     return app
